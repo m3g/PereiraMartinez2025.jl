926013c3b217dffbad108a5c245be3015fca0cff
Leandro Martinez <lmartine@unicamp.br>
2024-09-19 09:00:17 -0300

    add notebook

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,39 @@
 import Pkg
-Pkg.add("Documenter")
+Pkg.add([
+    "Documenter",
+    "Plots",
+    "MolSimToolkit",
+    "Unitful",
+    "EasyFit",
+    "Literate",
+    "PlutoStaticHTML",
+]
+)
 using Documenter
-push!(LOAD_PATH, "../src/")
+using Plots
+using MolSimToolkit
+using Unitful
+using EasyFit
+using Literate
+using PlutoStaticHTML: build_notebooks, BuildOptions, documenter_output
+
+# Building markdown from Pluto notebooks
+pluto_notebooks_dir = joinpath(@__DIR__, "src", "pluto_notebooks")
+bopts = BuildOptions(
+    pluto_notebooks_dir; 
+    output_format=documenter_output, 
+    previous_dir=pluto_notebooks_dir,
+)
+build_notebooks(bopts)
+
 makedocs(
-    #modules = [PereiraMartinez2024],
     sitename = "PereiraMartinez2024.jl",
+    format=Documenter.HTML(;
+        mathengine=Documenter.MathJax3(),
+    ),
     pages = [
         "Home" => "index.md",
+        "TMAO solutions" => "pluto_notebooks/sh3_tmao.md",
     ],
 )
 deploydocs(
